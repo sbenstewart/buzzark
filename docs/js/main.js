e1f3231b7762e92fa8ee09e81df2ae5fc33acdfb
sbenstewart <sbenstewart@gmail.com>
2019-12-10 09:17:13 +0530

    changing intro image

--- docs/js/main.js
+++ docs/js/main.js
@@ -100,6 +100,60 @@
     }
   });
 
+  /*var images = ['intro-img.svg','about-extra-1.svg'], 
+  index = 0, // starting index
+  maxImages = images.length - 1;
+  var timer = setInterval(function() {
+  var curImage = images[index];
+  index = (index == maxImages) ? 0 : ++index;
+  // set your image using the curImageVar 
+  $('div.intro-img img').attr('src','img/'+curImage);
+  }, 1000);*/
+
+var duration = 10; // duration in seconds
+var fadeAmount = 0.3; // fade duration amount relative to the time the image is visible
+
+$(document).ready(function (){
+  var images = $("#intro-img img");
+  var numImages = images.size();
+  var durationMs = duration * 1000;
+  var imageTime = durationMs / numImages; // time the image is visible 
+  var fadeTime = imageTime * fadeAmount; // time for cross fading
+  var visibleTime = imageTime  - (imageTime * fadeAmount * 2);// time the image is visible with opacity == 1
+  var animDelay = visibleTime * (numImages - 1) + fadeTime * (numImages - 2); // animation delay/offset for a single image 
+  
+  images.each( function( index, element ){
+    if(index != 0){
+      $(element).css("opacity","0");
+      setTimeout(function(){
+        doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
+      },visibleTime*index + fadeTime*(index-1));
+    }else{
+      setTimeout(function(){
+        $(element).animate({opacity:0},fadeTime, function(){
+          setTimeout(function(){
+            doAnimationLoop(element,fadeTime, visibleTime, fadeTime, animDelay);
+          },animDelay )
+        });
+      },visibleTime);
+    }
+  });
+});
+
+// creates a animation loop
+function doAnimationLoop(element, fadeInTime, visibleTime, fadeOutTime, pauseTime){
+  fadeInOut(element,fadeInTime, visibleTime, fadeOutTime ,function(){
+    setTimeout(function(){
+      doAnimationLoop(element, fadeInTime, visibleTime, fadeOutTime, pauseTime);
+    },pauseTime);
+  });
+}
+
+// shorthand for in- and out-fading
+function fadeInOut( element, fadeIn, visible, fadeOut, onComplete){
+  return $(element).animate( {opacity:1}, fadeIn ).delay( visible ).animate( {opacity:0}, fadeOut, onComplete);
+}
+
   // Navigation active state on scroll
   var nav_sections = $('section');
   var main_nav = $('.main-nav, .mobile-nav');
